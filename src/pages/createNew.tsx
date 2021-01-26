dc85a01e81281f3273ffab4a25e76753eeb8c4b5
Adnan <adnanqazi123@gmail.com>
2021-01-26 21:37:55 +0500

    btr

--- src/pages/createNew.tsx
+++ src/pages/createNew.tsx
@@ -4,9 +4,13 @@ import Header from "../component/Header"
 import Lolly from "../component/Lolly"
 import { useCreateLollyMutation } from "../generated/graphql";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { createLollyMutation } from "../query";
 import dotenv from 'dotenv'
 import { navigate } from "gatsby";
+=======
+import { createLollyMutation } from "./query";
+>>>>>>> parent of 4038ce5... btrb
 =======
 import { createLollyMutation } from "./query";
 >>>>>>> parent of 4038ce5... btrb
